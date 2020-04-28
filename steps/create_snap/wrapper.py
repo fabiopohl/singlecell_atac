f32791ab28dabde5accaaa3af6c9bdee20f33848
Fabio Pohl <fabio.baldivia.pohl@ki.se>
2020-04-28 17:38:14 +0200

    Changes in create snap wrapper

--- steps/create_snap/wrapper.py
+++ steps/create_snap/wrapper.py
@@ -23,7 +23,7 @@ shell("samtools view {bam_file} -H > {sample_name}.header.sam")
 shell(
 "cat <( cat {sample_name}.header.sam ) "
 "<( samtools view {bam_file} | awk '{{for (i=12; i<=NF; ++i) {{ if ($i ~ \"^CB:Z:\"){{ td[substr($i,1,2)] = substr($i,6,length($i)-5); }} }}; printf \"%s:%s\\n\", td[\"CB\"], $0 }}' ) "
-"| samtools view -bS - > {sample_nshme}.snap.bam"
+"| samtools view -bS - > {sample_name}.snap.bam"
 )
 
 # Sort bam file
@@ -31,20 +31,20 @@ shell("samtools sort -n -@ 10 -m 1G {sample_name}.snap.bam -o {sample_name}.snap
 
 # Generate snap file
 shell(
-        """snaptools snap-pre  
-	--input-file={sample_name}.snap.nsrt.bam  
-	--output-snap={sample_name}.snap  
-        --genome-name=hg38 
-        --genome-size=/Users/fabiopohl/proj/human_fetal_atac/hg38.chrom.sizes 
-	--min-mapq=30  
-	--min-flen=50  
-	--max-flen=1000  
-	--keep-chrm=TRUE  
-	--keep-single=FALSE  
-	--keep-secondary=False  
-	--overwrite=True  
-	--max-num=20000  
-	--min-cov=500  
-	--verbose=True"""
+    "snaptools snap-pre "  
+    "--input-file={sample_name}.snap.nsrt.bam " 
+    "--output-snap={sample_name}.snap "  
+    "--genome-name=hg38 " 
+    "--genome-size=/data/proj/GCB_FBP/human_atac_fetal/hg38.chrom.sizes "
+    "--min-mapq=30 "
+    "--min-flen=50 "
+    "--max-flen=1000 " 
+    "--keep-chrm=TRUE " 
+    "--keep-single=FALSE " 
+    "--keep-secondary=False " 
+    "--overwrite=True " 
+    "--max-num=20000 " 
+    "--min-cov=500 " 
+    "--verbose=True"
 )
 
